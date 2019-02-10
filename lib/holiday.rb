3d5889a309375d88a8c1f40e450a87ccd01d179e
abstract-animation-9914 <vm930@hotmail.com>
2019-02-10 22:29:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,8 +50,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-   holiday_hash[:winter].map do |holiday,supplies| 
-     supplies
+  holiday_hash[:winter].map do |holiday,supplies| 
+    supplies
       end.flatten
 end
 
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holidays|
   puts "#{season.capitalize}:"
   holidays.each do |holiday, supplies|
-     puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
     end 
   end
 end
@@ -75,11 +75,23 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.map do |season,holidays|
- holidays.map do |holiday,supplies|
- holiday if supplies.include?("BBQ")
+holidays.map do |holiday,supplies|
+holiday if supplies.include?("BBQ")
 end 
 end.flatten.compact
 end
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
